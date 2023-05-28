c56f75d10863965b72ee3810d0fb8f299d328f10
ppasszzw311 <ppasszzw311@gmail.com>
2023-05-28 14:23:54 +0800

    create component

--- 01.react-complete-guide/src/App.js
+++ 01.react-complete-guide/src/App.js
@@ -1,9 +1,17 @@
+import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+  const expenses = [
+    { title: 'Car Insurance', amount: 294.67, date: new Date(2021, 2, 28) },
+    { title: 'Toilet Paper', amount: 12.94, date: new Date(2021, 2, 28) },
+    { title: 'New TV', amount: 799.49, date: new Date(2021, 2, 28) },
+    { title: 'New Desk (Wooden)', amount: 450, date: new Date(2021, 2, 28) }
+  ];
   return (
     <div>
       <h2>Let's get started!</h2>
       <p>This is alsse le</p>
+      { expenses.map(expenses => <ExpenseItem title={expenses.title} amount={expenses.amount} date={expenses.date} />)}
     </div>
   )
 }
---
+++ 01.react-complete-guide/src/components/ExpenseDate.js
@@ -0,0 +1,22 @@
+import "./ExpenseDate.css"
+
+function ExpenseDate(props) {
+  const month = props.date.toLocaleString("en-US", { month: "long" });
+  const year = props.date.getFullYear();
+  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
+  return (
+    <div className="calender">
+      <div className="calender__month">
+        <p >{month}</p>
+      </div>
+      <div className="calender__year">
+        <p >{year}</p>
+      </div>
+      <div className="calender__day">
+        <p >{day}</p>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseDate;
